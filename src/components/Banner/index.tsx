a328de8f24531a216eb78a6aa9361c1982105fb6
João Paulo Santos de Souza <joaopaulo.santos_souza@hotmail.com>
2024-04-30 21:08:32 -0300

    adicionando botões no banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,14 +1,26 @@
 import * as S from './style'
 import BannerImg from '../../assets/images/banner-homem-aranha.png'
+import Tag from '../Tag'
+import Button from '../Button'
 
 const Banner = () => (
   <S.Imagem style={{ backgroundImage: `url(${BannerImg})` }}>
     <div className="container">
-      <S.Titulo>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</S.Titulo>
-      <S.Precos>
-        De <span>R$ 250,00</span> <br />
-        por apenas R$ 99,90
-      </S.Precos>
+      <Tag size="big">Destaque do dia</Tag>
+      <div>
+        <S.Titulo>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</S.Titulo>
+        <S.Precos>
+          De <span>R$ 250,00</span> <br />
+          por apenas R$ 99,90
+        </S.Precos>
+      </div>
+      <Button
+        type="link"
+        to="/produto"
+        title="Clique aqui para aproveitar essa oferta"
+      >
+        Aproveite
+      </Button>
     </div>
   </S.Imagem>
 )
